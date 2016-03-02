952841f513fba31727eb5d07a028d812cbf80a0e
Paul Solt <PaulSolt@gmail.com>
2016-03-02 14:32:44 -0500

    reverted back to older version

--- Test Project/Test Project/AppDelegate.swift
+++ Test Project/Test Project/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var x = 0
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
