e9026e9e11c253bdf8c5b438d5f4e2fcbee925ff
Paul Solt <PaulSolt@gmail.com>
2016-03-02 13:56:22 -0500

    Updated code 1

--- Test Project/Test Project/ViewController.swift
+++ Test Project/Test Project/ViewController.swift
@@ -16,8 +16,7 @@ class ViewController: UIViewController {
      
         print("label 1")
         
-        // call this twice
-        method()
+        // call this once
         method()
     }
     
